76f8458643a3497e38c0784068aba98be5af7d72
rjaneUCF <60888116+rjaneUCF@users.noreply.github.com>
2020-02-23 15:53:54 -0500

    Delete S20_T_MAX_Daily_Completed_Detrend_Declustered.R

--- data-raw/S20_T_MAX_Daily_Completed_Detrend_Declustered.R
+++
@@ -1,2 +0,0 @@
-S20_T_MAX_Daily_Completed_Detrend_Declustered<-read.csv("S20_T_MAX_Daily_Completed_Detrend_Declustered.csv",header=TRUE)
-devtools::use_data(S20_T_MAX_Daily_Completed_Detrend_Declustered)
