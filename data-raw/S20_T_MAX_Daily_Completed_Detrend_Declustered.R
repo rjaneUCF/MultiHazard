b2475442f7976ee36c4b7ca6d2342996aee9a838
rjaneUCF <60888116+rjaneUCF@users.noreply.github.com>
2020-05-20 20:53:45 -0400

    Update S20_T_MAX_Daily_Completed_Detrend_Declustered.R

--- data-raw/S20_T_MAX_Daily_Completed_Detrend_Declustered.R
+++ data-raw/S20_T_MAX_Daily_Completed_Detrend_Declustered.R
@@ -1,2 +1,2 @@
-S20_T_MAX_Daily_Completed_Detrend_Declustered<-read.csv("S20_T_MAX_Daily_Completed_Detrend_Declustered.csv",header=TRUE)[,c(1,3)]
+S20_T_MAX_Daily_Completed_Detrend_Declustered<-read.csv("S20_T_MAX_Daily_Completed_Detrend_Declustered.csv",header=TRUE)
 devtools::use_data(S20_T_MAX_Daily_Completed_Detrend_Declustered)
