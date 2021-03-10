55b29def956dfd1b46ec5b563a3814071f7cdc8e
rjaneUCF <60888116+rjaneUCF@users.noreply.github.com>
2021-03-09 20:14:03 -0500

    Add files via upload

---
+++ R/Vine_Copula_Fit.R
@@ -0,0 +1,21 @@
+#' C and D-vine Copula - Fitting
+#'
+#' Fit either a C- or D-vine copula model. Function is a repackaging the \code{RVineStructureSelect} and \code{RVineCopSelect} functions from the \code{RVine} package into a single function.
+#'
+#' @param Data Data frame containing \code{n} at least partially concurrent time series. First column may be a \code{"Date"} object. Can be \code{Dataframe_Combine} output.
+#' @return List comprising the vine copula \code{Structure}, pair-copula families composing the C- or D-vine copula \code{Family}, its parameters \code{Par} and \code{Par2}.
+#' @seealso \code{\link{Dataframe_Combine}} \code{\link{Vine_Copula_Sim}}
+#' @export
+#' @examples
+#' S20.Vine<-Vine_Copula_Fit(Data=S20.Detrend.df)
+Vine_Copula_Fit<-function(Data){
+  if(class(Data[,1])=="Date" | class(Data[,1])=="factor"){
+  M<-RVineStructureSelect(pobs(t))
+  Model <- RVineCopSelect(pobs(na.omit(Data[,2:ncol(Data)])),Matrix=M$Matrix)
+  } else {
+  M<-RVineStructureSelect(pobs(t))
+  Model <- RVineCopSelect(pobs(na.omit(Data[,1:ncol(Data)])),Matrix=M$Matrix)
+  }
+  res<- list(Structure = Model$Matrix, Family=Model$family, Par = Model$par, Par2 = Model$par2)
+  return(res)
+}
