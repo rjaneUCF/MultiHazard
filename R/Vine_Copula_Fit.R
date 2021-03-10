bef5294745ed758049ecddfe11f026e47c32f3cb
rjaneUCF <60888116+rjaneUCF@users.noreply.github.com>
2021-03-10 00:47:15 -0500

    Add files via upload

--- R/Vine_Copula_Fit.R
+++ R/Vine_Copula_Fit.R
@@ -10,10 +10,10 @@
 #' S20.Vine<-Vine_Copula_Fit(Data=S20.Detrend.df)
 Vine_Copula_Fit<-function(Data){
   if(class(Data[,1])=="Date" | class(Data[,1])=="factor"){
-  M<-RVineStructureSelect(pobs(t))
+  M<-RVineStructureSelect(pobs(na.omit(Data[,2:ncol(Data)])))
   Model <- RVineCopSelect(pobs(na.omit(Data[,2:ncol(Data)])),Matrix=M$Matrix)
   } else {
-  M<-RVineStructureSelect(pobs(t))
+  M<-RVineStructureSelect(pobs(na.omit(Data[,1:ncol(Data)]))
   Model <- RVineCopSelect(pobs(na.omit(Data[,1:ncol(Data)])),Matrix=M$Matrix)
   }
   res<- list(Structure = Model$Matrix, Family=Model$family, Par = Model$par, Par2 = Model$par2)
