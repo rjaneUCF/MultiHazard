b0902cc6967d9f05ec46b02ea2ec0450e1d1c145
rjaneUCF <60888116+rjaneUCF@users.noreply.github.com>
2021-03-16 11:28:14 -0400

    Add files via upload

--- R/GPD_Threshold_Solari_Sel.R
+++ R/GPD_Threshold_Solari_Sel.R
@@ -8,7 +8,7 @@
 #' \item \code{2} Numeric vector containing corresponding time series values.
 #' }
 #' @param Solari_Output Output of the \code{GPD_Threshold_Solari} function.
-#' @param u Numeric vector of length one specifying the threshold to analyze, chosen by the user based on plots from the \code{GPD_Threshold_Solari} function.
+#' @param Thres Numeric vector of length one specifying the threshold to analyze, chosen by the user based on plots from the \code{GPD_Threshold_Solari} function.
 #' @param Alpha Numeric vector of length one specifying the level of confidence associated with the confidence interval i.e., the probability that the interval contains the true value of the parameter is \eqn{1-\frac{Alpha}{2}}. The interval is referred to as the \eqn{100(1-\frac{Alpha}{2})\%} confidence interval. Default is \code{0.1}.
 #' @param N_Sim Numeric vector of length one specifying the size of the samples used in the bootstrap simulation. Default is \code{10^4}.
 #' @param RP_Max Numeric vector of length one specifying the maximum return level to be calculated. Default is \code{1000}.
@@ -17,19 +17,19 @@
 #' @param y_lab Character vector specifying the y-axis label of the return level plot.
 #' @return List containing three objects: \code{Estimate}, \code{CI_Upper} and \code{CI_Lower}. The \code{Estimate} dataframe comprises \itemize{
 #' \item \code{xi}
-#' GPD shape parameter estimate for the threshold is \code{u}.
+#' GPD shape parameter estimate for the threshold is \code{Thres}.
 #' \item \code{sigma}
-#' GPD scale parameter estimate for the threshold is \code{u}.
-#' \item \code{u}
-#' GPD location parameter estimate for the threshold is \code{u}.
+#' GPD scale parameter estimate for the threshold is \code{Thres}.
+#' \item \code{Thres}
+#' GPD location parameter estimate for the threshold is \code{Thres}.
 #' \item \code{rate}
-#' GPD rate parameter i.e., number of independent excesses per year for a threshold of \code{u}.
+#' GPD rate parameter i.e., number of independent excesses per year for a threshold of \code{Thres}.
 #' \item The remaining columns are \code{RL}
-#' Return level estimates from the GPD using a threshold of \code{u}.
+#' Return level estimates from the GPD using a threshold of \code{Thres}.
 #' }
 #' \code{CI_Upper} and \code{CI_Lower} give the upper and lower bounds of the \eqn{100(1-\frac{Alpha}{2})\%} confidence interval for the corresponding element in \code{Estimate}.
-#' Top row: Histograms of the GPD parameter estimates based on a nonparametric bootstrapping simulation. Grey bars correspond to the estimates obtained as the threshold (\code{u}) is varied, found by running the function a necessary input of the function. Continuous black lines correspond to results obtained by fixing the threshold at \code{u}. Dashed blue lines correspond to the expected values for the fixed threshold.
-#' Lower left: Return level plot. Return levels of the observations estimated from the empirical distribution. Grey bars correspond to the maximum of the upper and lower bounds of the \eqn{100(1-\frac{Alpha}{2})\%} confidence intervals as the threshold is varied. Continuous black lines correspond to results obtained by fixing the threshold at \code{u}. Dashed blue lines correspond to the expected values for the fixed threshold.
+#' Top row: Histograms of the GPD parameter estimates based on a nonparametric bootstrapping simulation. Grey bars correspond to the estimates obtained as the threshold (\code{Thres}) is varied, found by running the function a necessary input of the function. Continuous black lines correspond to results obtained by fixing the threshold at \code{Thres}. Dashed blue lines correspond to the expected values for the fixed threshold.
+#' Lower left: Return level plot. Return levels of the observations estimated from the empirical distribution. Grey bars correspond to the maximum of the upper and lower bounds of the \eqn{100(1-\frac{Alpha}{2})\%} confidence intervals as the threshold is varied. Continuous black lines correspond to results obtained by fixing the threshold at \code{Thres}. Dashed blue lines correspond to the expected values for the fixed threshold.
 #' Lower right: As in the top row but for the 100 years return period quantile.
 #' @export
 #' @examples
@@ -39,7 +39,7 @@
 #' S22_OsWL_Solari<-GPD_Threshold_Solari(Event=Rainfall_Declust_SW$Declustered,
 #'                                   Data=na.omit(S22.Detrend.df[,c(1:2)]))
 #'
-GPD_Threshold_Solari_Sel<-function(Event,Data,Solari_Output,u,Alpha=0.1,N_Sim=10^4,RP_Max=1000,RP_Plot=100,mu=365.25,y_lab="Data"){
+GPD_Threshold_Solari_Sel<-function(Event,Data,Solari_Output,Thres,Alpha=0.1,N_Sim=10^4,RP_Max=1000,RP_Plot=100,mu=365.25,y_lab="Data"){
 
   # Auxiliary variables
   Data    = na.omit(Data)
@@ -51,9 +51,9 @@ GPD_Threshold_Solari_Sel<-function(Event,Data,Solari_Output,u,Alpha=0.1,N_Sim=10
   # Sort Events
   Event = sort(Event)
 
-  for(J in 1:length(u)){
+  for(J in 1:length(Thres)){
 
-    Exceedence = Event[Event>u[J]]
+    Exceedence = Event[Event>Thres[J]]
     Rate = length(Exceedence)/N_Years
 
     Estimate   = unlist(GPD_MLE_Boot(Exceedence,RP,N_Years))
@@ -75,7 +75,7 @@ GPD_Threshold_Solari_Sel<-function(Event,Data,Solari_Output,u,Alpha=0.1,N_Sim=10
   layout(layout_matrix)
 
   # Histogram of k (GPD shape)
-  z<-which(Solari_Output$GPD_MLE[,1]>-0.5 & Solari_Output$GPD_MLE[,1]<0.5 & Solari_Output$GPD_MLE[,6]<(length(na.omit(Data[,2])/Mu)))
+  z<-which(Solari_Output$GPD_MLE[,1]>-0.5 & Solari_Output$GPD_MLE[,1]<0.5 & Solari_Output$GPD_MLE[,6]<(length(na.omit(Data[,2])/mu)))
   h<-hist(Solari_Output$GPD_MLE[z,1],xlab="GP shape",ylab="Frequency",col="Grey",
           xlim=c(min(Solari_Output$GPD_MLE[z,1])-diff(range(Solari_Output$GPD_MLE[z,1]))/4,
                  max(Solari_Output$GPD_MLE[z,1])+diff(range(Solari_Output$GPD_MLE[z,1]))/4),
@@ -121,7 +121,7 @@ GPD_Threshold_Solari_Sel<-function(Event,Data,Solari_Output,u,Alpha=0.1,N_Sim=10
   # Histogram for a return level of 100 years
   Solari_Output_RPs<-as.numeric(colnames(Solari_Output$GPD_MLE)[7:ncol(Solari_Output$GPD_MLE)])
   Solari_Output_RPs<-Solari_Output_RPs[which(Solari_Output_RPs<=RP_Max)]
-  plot(log10(1/((1-(1:length(Exceedence))/(length(Exceedence)+1))*Rate)),sort(Event[which(Event>u)]),
+  plot(log10(1/((1-(1:length(Exceedence))/(length(Exceedence)+1))*Rate)),sort(Event[which(Event>Thres)]),
        xlab=expression('log'[10]*'(Return period) [years]'),ylab=y_lab,
        xlim=c(log10(1),log10(RP_Max)),
        ylim=c(min(Event,min(apply(Solari_Output$CI_Lower[z,c(7:(6+length(Solari_Output_RPs)))],2,min))),max(apply(Solari_Output$CI_Upper[z,rev(c(7:(6+length(Solari_Output_RPs))))],2,max))),
@@ -132,7 +132,7 @@ GPD_Threshold_Solari_Sel<-function(Event,Data,Solari_Output,u,Alpha=0.1,N_Sim=10
   lines(log10(RP),Estimate[-(1:4)],col="Blue",lwd=2)
   lines(log10(RP),CI.Upper[-(1:4)],lwd=2)
   lines(log10(RP),CI.Lower[-(1:4)],lwd=2)
-  points(log10(1/((1-(1:length(Exceedence))/(length(Exceedence)+1))*Rate)),sort(Event[which(Event>u)]),col="Green",pch=16)
+  points(log10(1/((1-(1:length(Exceedence))/(length(Exceedence)+1))*Rate)),sort(Event[which(Event>Thres)]),col="Green",pch=16)
 
   h<-hist(Solari_Output$GPD_MLE[z,which(colnames(Solari_Output$GPD_MLE)==RP_Plot)],
           xlab=paste(RP_Plot,'year return level',colnames(Data)[2]),ylab="Frequency",col="Grey",
@@ -148,7 +148,7 @@ GPD_Threshold_Solari_Sel<-function(Event,Data,Solari_Output,u,Alpha=0.1,N_Sim=10
               max(Solari_Output$GPD_MLE[z,which(colnames(Solari_Output$GPD_MLE)==RP_Plot)])+diff(range(Solari_Output$GPD_MLE[z,which(colnames(Solari_Output$GPD_MLE)==RP_Plot)]))/4),
        lwd=2,xlab="",ylab="",xaxt='n',yaxt='n')
 
-  names(Estimate)<-c("xi","sigma","u","rate",as.character(RP))
+  names(Estimate)<-c("xi","sigma","Thres","rate",as.character(RP))
   res<-list(Estimate=Estimate,CI_Lower=CI.Lower,CI_Upper=CI.Upper)
   return(res)
 }
