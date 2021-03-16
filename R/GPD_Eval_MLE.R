d3132a0648172d537952e9ffd6d673ee223ed774
rjaneUCF <60888116+rjaneUCF@users.noreply.github.com>
2021-03-16 12:50:57 -0400

    Add files via upload

---
+++ R/GPD_Eval_MLE.R
@@ -0,0 +1,20 @@
+#' @noRd
+GPD_Eval_MLE<-function(Data,RP,Years){
+  # parameters
+  mod = evm(na.omit(Data), th = quantile(Data, 0))
+  xi = summary(mod)$coef[[2]]
+  sigma = exp(summary(mod)$coef[[1]])
+  u = as.numeric(quantile(Data,0))
+  # MRLP
+  MRLP       = mean(Data - u)
+  # Modifiewd shape parameter
+  mod_sigma   = sigma - xi*u
+  # Number of events per year
+  rate         = length(Data)/Years
+  # Return period
+  PRI        = 1-1/(RP*rate)
+  RP_Est        = qgpd(PRI,xi=xi,sigma=sigma,u=u)
+  # Output
+  res        = list(xi=xi,sigma=sigma,u=u,MRLP=MRLP,mod_sigma=mod_sigma,rate=rate,RP_Est=RP_Est)
+  return(res)
+}
