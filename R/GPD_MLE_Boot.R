dbfec55c5649c2a28f15184fbd7c69bc0b4f5215
rjaneUCF <60888116+rjaneUCF@users.noreply.github.com>
2021-03-16 12:03:56 -0400

    Add files via upload

--- R/GPD_MLE_Boot.R
+++ R/GPD_MLE_Boot.R
@@ -1,12 +1,12 @@
 #' @noRd
 GPD_MLE_Boot<-function(Data,RP,Years){
   mod        = GPD_MLE(Data)
-  xi         = mod$K
-  sigma      = mod$SIG
+  xi         = mod$xi
+  sigma      = mod$sigma
   u          = mod$u
   rate       = length(Data)/Years
   PRI        = 1-1/(RP*rate)
-  RP_Est     = qgpd(PRI,xi=K,sigma=SIG,u=u)
+  RP_Est     = qgpd(PRI,xi=xi,sigma=sigma,u=u)
   res        = list(xi=xi,sigma=sigma,u=u,rate=rate,RP_Est=RP_Est)
   return(res)
 }
