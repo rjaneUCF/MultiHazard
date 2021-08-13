22eab202b7c52edcb1be8d52d8ff4bec93afdcdb
rjaneUCF <60888116+rjaneUCF@users.noreply.github.com>
2021-08-13 14:49:24 -0400

    Add files via upload

--- R/GDP_MLE.R
+++ R/GDP_MLE.R
@@ -1,8 +1,8 @@
 #' @noRd
 GPD_MLE<-function(Data){
   mod = evm(na.omit(Data), th = quantile(Data, 0))
-  xi = summary(mod)$coef[[2]]
-  sigma = exp(summary(mod)$coef[[1]])
+  xi = mod[[1]][2]
+  sigma = exp(mod[[1]][1])
   u = as.numeric(quantile(Data,0))
   res<-list(xi=xi,sigma=sigma,u=u)
   return(res)
