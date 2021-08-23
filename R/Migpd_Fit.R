2102a31aa4f87795dabe2ea1a912e750f3513868
rjaneUCF <60888116+rjaneUCF@users.noreply.github.com>
2021-08-23 15:53:59 -0400

    Add files via upload

--- R/Migpd_Fit.R
+++ R/Migpd_Fit.R
@@ -18,7 +18,7 @@
 #' S22.GPD<-Migpd_Fit(Data=S22.Detrend.Declustered.df, mqu =c(0.99,0.99,0.99))
 #' #Without date as first column
 #' S22.GPD<-Migpd_Fit(Data=S22.Detrend.Declustered.df[,-1], mqu =c(0.99,0.99,0.99))
-Migpd_Fit<-function (Data, Data_Full=NA, mth=NA, mqu, penalty = "gaussian", maxit = 10000,
+Migpd_Fit<-function (Data, Data_Full=NA, mth, mqu, penalty = "gaussian", maxit = 10000,
                       trace = 0, verbose = FALSE, priorParameters = NULL){
 
   if(class(Data[,1])=="Date" | class(Data[,1])=="factor"){
@@ -27,7 +27,7 @@ Migpd_Fit<-function (Data, Data_Full=NA, mth=NA, mqu, penalty = "gaussian", maxi
   data = Data
   }
 
-  if(is.na(mth)==T){
+  if(missing(mth)){
   if(class(Data_Full[,1])=="Date" | class(Data_Full[,1])=="factor"){
     data_full<-Data_Full[,-1]
   } else {
