9f18006c2138fb34f9296d4645e74ba7edf37100
rjaneUCF <60888116+rjaneUCF@users.noreply.github.com>
2022-04-28 13:12:25 -0400

    Update Design_Event_2D.R

--- R/Design_Event_2D.R
+++ R/Design_Event_2D.R
@@ -315,7 +315,7 @@ Design_Event_2D<-function(Data, Data_Con1, Data_Con2, u1, u2, Thres1=NA, Thres2=
     cdf.MX<-pMX(xx, mu=list(mu1=exp(marginal_non_con2$models[[1]]$mu.coefficients), mu2=exp(marginal_non_con2$models[[2]]$mu.coefficients)),
                 sigma=list(sigma1=exp(marginal_non_con2$models[[1]]$sigma.coefficients), sigma2=exp(marginal_non_con2$models[[2]]$sigma.coefficients)),
                 pi = list(pi1=prob.MX1, pi2=prob.MX2), family=list(fam1="GA", fam2="GA"))
-    cop.sample2.non.con <- approx(x=cdf.MX,y= xx, xout=sample[,con1])$y
+    cop.sample2.non.con <- approx(cdf.MX, xx, xout=sample[,con1])$y
   }
   if(Marginal_Dist2=="GamMix(3)"){
     xx <- seq(0, max(Data_Con1[,con1])*10, 0.001)
@@ -325,7 +325,7 @@ Design_Event_2D<-function(Data, Data_Con1, Data_Con2, u1, u2, Thres1=NA, Thres2=
     cdf.MX<-pMX(xx, mu=list(mu1=exp(marginal_non_con2$models[[1]]$mu.coefficients), mu2=exp(marginal_non_con2$models[[2]]$mu.coefficients), mu3=exp(marginal_non_con2$models[[3]]$mu.coefficients)),
                 sigma=list(sigma1=exp(marginal_non_con2$models[[1]]$sigma.coefficients), sigma2=exp(marginal_non_con2$models[[2]]$sigma.coefficients), sigma3=exp(marginal_non_con2$models[[3]]$sigma.coefficients)),
                 pi = list(pi1=prob.MX1, pi2=prob.MX2, pi3=prob.MX3), family=list(fam1="GA", fam2="GA", fam3="GA"))
-    cop.sample2.non.con <- approx(x=cdf.MX,y= xx, xout=sample[,con1])$y
+    cop.sample2.non.con <- approx(cdf.MX, xx, xout=sample[,con1])$y
   }
   if(Marginal_Dist2=="Gaus"){
     cop.sample2.non.con<-qnorm(sample[,con1], mean = as.numeric(marginal_non_con2$estimate[1]), sd=as.numeric(marginal_non_con2$estimate[2]))
