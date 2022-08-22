df12bcf99ac7450e70cf0cb0dae2dcde09995ebe
rjaneUCF <60888116+rjaneUCF@users.noreply.github.com>
2022-08-22 18:22:40 -0400

    Update Design_Event_2D.R

--- R/Design_Event_2D.R
+++ R/Design_Event_2D.R
@@ -834,7 +834,7 @@ Design_Event_2D<-function(Data, Data_Con1, Data_Con2, u1, u2, Thres1=NA, Thres2=
       FullDependence[[k]]<-FullDependence.AND
       #Generate a sample of events along the contour. Sample is weighted according to the probabilities
       #given by the KDE estimate for each point on the isoline. Sample size is N_Ensemble.
-      sample.AND <- NA #Iso[sample(1:length(prediction[prediction>0]),size = N_Ensemble, replace = TRUE, prob=prediction[prediction>0]),]
+      sample.AND <- Iso[sample(1:length(prediction[prediction>0]),size = N_Ensemble, replace = TRUE, prob=prediction[prediction>0]),]
       colnames(sample.AND) <- c(names(Data)[1],names(Data)[2])
       #Put the ensemble of design event into a data frame to form part of the function's output.
       Ensemble[[k]] <- data.frame(sample.AND)
