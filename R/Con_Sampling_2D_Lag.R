a445273d8362fc5c935765cbfd8f60fb9b539f78
rjaneUCF <60888116+rjaneUCF@users.noreply.github.com>
2023-03-30 21:20:46 -0400

    Add files via upload

--- R/Con_Sampling_2D_Lag.R
+++ R/Con_Sampling_2D_Lag.R
@@ -1,22 +1,22 @@
 #' Conditionally sampling a two dimensional dataset
 #'
-#' Creates a data frame where the declustered excesses of a (conditioning) variable are paired with the maximum value of a second variable over a specified lag.
+#' Creates a data frame where the declustered excesses of a (conditioning) variable are paired with the maximum value of a second variable over a specified time-lag.
 #'
 #' @param Data_Detrend Data frame containing two at least partially concurrent time series, detrended if necessary. Time steps must be equally spaced, with missing values assigned \code{NA}. First object may be a \code{"Date"} object. Can be \code{Dataframe_Combine} output.
 #' @param Data_Declust Data frame containing two (independently) declustered at least partially concurrent time series. Time steps must be equally spaced, with missing values assigned \code{NA}. Columns must be in the same order as in \code{Data_Detrend}. First object may be a \code{"Date"} object. Can be \code{Dataframe_Combine} output.
 #' @param Con_Variable Column number (1 or 2) or the column name of the conditioning variable. Default is \code{1}.
 #' @param u Threshold, as a quantile of the observations of the conditioning variable. Default is \code{0.97}.
 #' @param Thres Threshold expressed on the original scale of the observations. Only one of \code{u} and \code{Thres} should be supplied. Default is \code{NA}.
-#' @param Lag_Backward Positieve lag applied to variable not assigned as the \code{Con_Variable}. Default is \code{0}
-#' @param Lag_Forward Negative lag to variable not assigned as the \code{Con_Variable}. Default is \code{0}
-#' @return List comprising the specified \code{Threshold} as the quantile of the conditioning variable above which declustered excesses are paired with co-occurences of the other variable, the resulting two-dimensional sample \code{data} and \code{name} of the conditioning variable. The index of the input dataset that correspond to the events of the conditioning variable \code{x.con} and the non-conditioning variable \code{x.noncon} in the conditonal sample are also provided.
+#' @param Lag_Backward Positive lag applied to variable not assigned as the \code{Con_Variable}. Default is \code{3}
+#' @param Lag_Forward Negative lag to variable not assigned as the \code{Con_Variable}. Default is \code{3}
+#' @return List comprising the specified \code{Threshold} as the quantile of the conditioning variable above which declustered excesses are paired with co-occurences of the other variable, the resulting two-dimensional sample \code{data} and \code{Con_Variable} the name of the conditioning variable. The index of the input dataset that correspond to the events of the conditioning variable \code{x.con} and the non-conditioning variable \code{x.noncon} in the conditonal sample are also provided.
 #' @export
 #' @examples
 #' S20.Rainfall<-Con_Sampling_2D(Data_Detrend=S20.Detrend.df[,-c(1,4)],
 #'                               Data_Declust=S20.Detrend.Declustered.df[,-c(1,4)],
 #'                               Con_Variable="Rainfall",u=0.97)
 Con_Sampling_2D_Lag<-
-  function (Data_Detrend, Data_Declust, Con_Variable, u = 0.97, Thres, Lag_Backward = 0, Lag_Forward = 0)
+  function (Data_Detrend, Data_Declust, Con_Variable, u = 0.97, Thres, Lag_Backward = 3, Lag_Forward = 3)
   {
     if (class(Data_Detrend[, 1]) == "Date" | class(Data_Detrend[,1]) == "factor" | class(Data_Detrend[,1])[1]=="POSIXct") {
       Data_Detrend <- Data_Detrend[, -1]
