40fb091369cb96298bcb023760b0f0a5073012b7
Robert Jane <60888116+rjaneUCF@users.noreply.github.com>
2025-02-14 15:20:31 -0500

    Add files via upload

--- R/rc_est.R
+++ R/rc_est.R
@@ -7,16 +7,16 @@
 #' @param rp Numeric vector of length one specifying return period of interest.
 #' @param mu Numeric vector of length one specifying the (average) occurrence frequency of events in Data. Default is 365.25, daily data.
 #' @param n_sim Numeric vector of length one specifying the number of simulations for HT model. Default is \code{50}.
-#' @param n_grad Numeric vector of length one specifying number of number of rays along which to compute points on the curve. Default is \code{50}. 
-#' @param boot_method Character vector of length one specifying the bootstrap method. Options are \code{"basic"} (default), \code{"block"} or \code{"monthly"}. 
-#' @param boot_replace Character vector of length one specifying whether bootstrapping is carried out with \code{"T"} or without \code{"F"} replacement. Only required if \code{boot_method = "basic"}. Default is \code{NA}. 
-#' @param block_length Numeric vector of length one specifying block length. Only required if \code{boot_method = "block"}. Default is \code{NA}. 
+#' @param n_grad Numeric vector of length one specifying number of number of rays along which to compute points on the curve. Default is \code{50}.
+#' @param boot_method Character vector of length one specifying the bootstrap method. Options are \code{"basic"} (default), \code{"block"} or \code{"monthly"}.
+#' @param boot_replace Character vector of length one specifying whether bootstrapping is carried out with \code{"T"} or without \code{"F"} replacement. Only required if \code{boot_method = "basic"}. Default is \code{NA}.
+#' @param block_length Numeric vector of length one specifying block length. Only required if \code{boot_method = "block"}. Default is \code{NA}.
 #' @param boot_prop Numeric vector of length one specifying the minimum proportion of non-missing values of at least of the variables for a month to be included in the bootstrap. Only required if \code{boot_method = "monthly"}. Default is \code{0.8}.
-#' @param n_boot Numeric vector of length one specifying number of bootstrap samples. Default is \code{100}. 
-#' @param decl_method_x Character vector of length one specifying the declustering method to apply to the first variable. Options are the storm window approach \code{"window"} (default) and the runs method \code{"runs"}. 
-#' @param decl_method_y Character vector of length one specifying the declustering method to apply to the second variable. Options are the storm window approach \code{"window"} (default) and the runs method \code{"runs"}. 
-#' @param window_length_x Numeric vector of length one specifying the storm window length to apply during the declustering of the  first variable if \code{decl_method_x = "window"}. 
-#' @param window_length_y Numeric vector of length one specifying the storm window length to apply during the declustering of the second variable if \code{decl_method_y = "window"}. 
+#' @param n_boot Numeric vector of length one specifying number of bootstrap samples. Default is \code{100}.
+#' @param decl_method_x Character vector of length one specifying the declustering method to apply to the first variable. Options are the storm window approach \code{"window"} (default) and the runs method \code{"runs"}.
+#' @param decl_method_y Character vector of length one specifying the declustering method to apply to the second variable. Options are the storm window approach \code{"window"} (default) and the runs method \code{"runs"}.
+#' @param window_length_x Numeric vector of length one specifying the storm window length to apply during the declustering of the  first variable if \code{decl_method_x = "window"}.
+#' @param window_length_y Numeric vector of length one specifying the storm window length to apply during the declustering of the second variable if \code{decl_method_y = "window"}.
 #' @param u_x Numeric vector of length one specifying the threshold to adopt in the declustering of the first variable if \code{decl_method_x = "runs"}. Default is \code{NA}.
 #' @param u_y Numeric vector of length one specifying the threshold to adopt in the declustering of the second variable if \code{decl_method_y = "runs"}. Default is \code{NA}.
 #' @param sep_crit_x Numeric vector of length one specifying the separation criterion to apply during the declustering of the first variable if \code{decl_method_x = "runs"}. Default is \code{NA}.
@@ -36,8 +36,8 @@
 #' @examples
 #' #Data starts on first day of 1948
 #' head(S22.Detrend.df)
-#' 
-#' #Dataframe ends on 1948-02-03 
+#'
+#' #Dataframe ends on 1948-02-03
 #' tail(S22.Detrend.df)
 #'
 #' #Adding dates to complete final month of combined records
@@ -46,22 +46,22 @@
 #' S22.Detrend.df.extended = rbind(S22.Detrend.df,final.month)
 #' #Derive return curves
 #' rc_est(data=S22.Detrend.df.extended[,1:3],q=0.985,rp=100,mu=365.25,n_sim=100,
-#'        n_grad=50,n_boot=100,boot_method="monthly", 
-#'        boot_replace=NA, block_length=NA, boot_prop=0.8, 
-#'        decl_method_x="runs", decl_method_y="runs", 
-#'        window_length_x=NA,window_length_y=NA, 
-#'        u_x=0.95, u_y=0.95, 
-#'        sep_crit_x=36, sep_crit_y=36, 
+#'        n_grad=50,n_boot=100,boot_method="monthly",
+#'        boot_replace=NA, block_length=NA, boot_prop=0.8,
+#'        decl_method_x="runs", decl_method_y="runs",
+#'        window_length_x=NA,window_length_y=NA,
+#'        u_x=0.95, u_y=0.95,
+#'        sep_crit_x=36, sep_crit_y=36,
 #'        alpha=0.1, x_lab=NA, y_lab=NA)
 rc_est = function(data,q,rp,mu,n_sim,n_grad,n_boot,boot_method, boot_replace, block_length, boot_prop, decl_method_x, decl_method_y, window_length_x, window_length_y, u_x=NA, u_y=NA, sep_crit_x=NA, sep_crit_y=NA, alpha=0.1, x_lab=colnames(data)[2], y_lab=colnames(data)[2],x_lim_min=min(data[,2],na.rm=T),x_lim_max=max(data[,2],na.rm=T)+0.3*diff(range(data[,2],na.rm=T)),y_lim_min=min(data[,3],na.rm=T),y_lim_max=max(data[,3],na.rm=T)+0.3*diff(range(data[,3],na.rm=T)),plot=T){
 
  #Convert return period (rp) to probability
  prob = (1/mu) / rp
- 
- #List to output the bootstrapped curves 
+
+ #List to output the bootstrapped curves
  boot_curves = list()
  boot_curves2 = list()
- 
+
 for(j in 1:n_boot){
 
  #Bootstrap data using user specified method
@@ -69,107 +69,107 @@ for(j in 1:n_boot){
   index = sample(1:nrow(data),replace=boot_replace)
   boot_df = data[index,]
  }
-  
+
  if(boot_method=="block"){
   boot_df = bootstrap_block(data,k=block_length)
  }
-  
+
  if(boot_method=="monthly"){
    boot_df = bootstrap_month(data,boot_prop)
  }
-  
+
  #Decluster bootstrapped series
  #Variable 1
   if(decl_method_x == "window"){
   decl = Decluster_SW(Data=boot_df[,1:2], Window_Width = window_length_x)
   boot_x_dec_df = data.frame(as.Date(data[,1]),decl$Declustered)
  }
-  
+
  if(decl_method_x == "runs"){
   decl = Decluster(Data=boot_df[,2], u=u_x, SepCrit = sep_crit_x, mu = mu)
   boot_x_dec_df = data.frame(as.Date(data[,1]),decl$Declustered)
  }
- 
+
  colnames(boot_x_dec_df) = colnames(data[,1:2])
  boot_x_dec_df$Date = as.Date(boot_x_dec_df$Date)
-  
+
  #Variable 2
  if(decl_method_y == "window"){
    decl = Decluster_SW(Data=boot_df[,c(1,3)], Window_Width = window_length_y)
    boot_y_dec_df = data.frame(as.Date(data[,1]),decl$Declustered)
  }
- 
+
  if(decl_method_y == "runs"){
   decl = Decluster(Data=boot_df[,3], u = u_y, SepCrit = sep_crit_y, mu = mu)
   boot_y_dec_df = data.frame(as.Date(data[,1]),decl$Declustered)
  }
-  
+
  colnames(boot_y_dec_df) = colnames(data[,c(1,3)])
  boot_y_dec_df$Date = as.Date(boot_y_dec_df$Date)
-  
+
  #Dataframes
  df = data.frame("Date"=seq.Date(as.Date(min(data$Date,data$Date)),as.Date(max(data$Date,data$Date)),by="day"))
  df_1 = left_join(df,boot_x_dec_df,by="Date")
  boot_decl_df = left_join(df_1,boot_y_dec_df,by="Date")
  colnames(boot_decl_df) = colnames(data)
-  
- #Dataframes to which HT04 model is applied 
+
+ #Dataframes to which HT04 model is applied
  data_x = data.frame(boot_decl_df[,2],boot_df[,3])
  data_y = data.frame(boot_df[,2],boot_decl_df[,3])
-  
+
  colnames(data_x) = colnames(data[,2:3])
  colnames(data_y) = colnames(data[,2:3])
- 
+
  #Fit GPDs
  thresh_x = quantile(boot_df[,2],q,na.rm=T)
  gpd_x = evm(data_x[,1],th = thresh_x)
- gpd_x$rate = length(data_x[,1][data_x[,1]>thresh_x])/length(na.omit(data_x[,1])/365.25)
- 
+ gpd_x$rate = length(data_x[,1][data_x[,1]>thresh_x])/(length(na.omit(data_x[,1]))/mu))
+
  thresh_y = quantile(boot_df[,3],q,na.rm=T)
  gpd_y = evm(data_y[,2],th = thresh_y)
- gpd_y$rate = length(data_y[,2][data_y[,2]>thresh_y])/length(na.omit(data_y[,2])/365.25)
-  
+ gpd_y$rate = length(data_y[,2][data_y[,2]>thresh_y])/(length(na.omit(data_y[,2]))/mu)
+
  data_x = na.omit(data_x)
  data_y = na.omit(data_y)
 
  #Converting to uniform scale
  data_unif_x = cbind(EmpFun(x = boot_df[,2],r = data_x[,1],mod = gpd_x),EmpFun(x = boot_df[,3], r = data_x[,2], mod = gpd_y))
  data_unif_y = cbind(EmpFun(x = boot_df[,2],r = data_y[,1],mod = gpd_x),EmpFun(x = boot_df[,3], r = data_y[,2], mod = gpd_y))
- 
+
  data_exp_x = apply(data_unif_x, 2, qexp)
  data_exp_y = apply(data_unif_y, 2, qexp)
 
  #estimated curves exponential margins with HT model
  curve = heff_tawn_curve_exp(data_exp_x = data_exp_x, data_exp_y = data_exp_y, prob = prob, q=q, nsim=n_sim)
-  
+
  #estimated curves uniform margins
  curve_unif = apply(curve, 2, pexp)
-  
+
  #estimated curves original margins
  curve_original = cbind(revTransform(x=curve_unif[,1],data = na.omit(boot_df[,2]),qu=q,th=thresh_x,exp(coefficients(gpd_x)[1]),coefficients(gpd_x)[2]),revTransform(x=curve_unif[,2],data = na.omit(boot_df[,3]),qu=q,th=thresh_y,exp(coefficients(gpd_y)[1]),coefficients(gpd_y)[2]))
-  
+
  boot_curves[[j]] = curve_original
-  
+
  #estimated curves exponential margins with WT model
  curve2.1 = wads_tawn_curve_exp(data_exp = data_exp_x,prob=prob,q=q)
  curve2.2 = wads_tawn_curve_exp(data_exp = data_exp_y,prob=prob,q=q)
-  
+
  rad2.1 = apply(curve2.1,1,function(x){sqrt(sum(x^2))})
  rad2.2 = apply(curve2.2,1,function(x){sqrt(sum(x^2))})
-  
+
  cons_curve = curve2.1
- 
+
  cons_curve[rad2.1>rad2.2,] = curve2.1[rad2.1>rad2.2,]
  cons_curve[rad2.1<=rad2.2,] = curve2.2[rad2.1<=rad2.2,]
-  
+
  #estimated curves uniform margins
  curve_unif2 = apply(cons_curve, 2, pexp)
-  
+
  #estimated curves original margins
  curve_original2 = cbind(revTransform(x=curve_unif2[,1],data = na.omit(boot_df[,2]),qu=q,th=thresh_x,exp(coefficients(gpd_x)[1]),coefficients(gpd_x)[2]),revTransform(x=curve_unif2[,2],data = na.omit(boot_df[,3]),qu=q,th=thresh_y,exp(coefficients(gpd_y)[1]),coefficients(gpd_y)[2]))
-  
+
  boot_curves2[[j]] = curve_original2
- 
+
 }
 
 angles = ((n_grad:1)/(n_grad+1))*(pi/2)
@@ -182,7 +182,7 @@ angles_est_points2 = array(0,dim=c(n_grad,2,n_boot))
 #Finding interceptions of curves with rays
 for(k in 1:n_boot){
   curve_angles = atan((boot_curves[[k]][,2]-y0)/(boot_curves[[k]][,1]-x0))
-  
+
   for(i in 1:n_grad){
     ind = min(which(angles[i] >= curve_angles))
     x1 = boot_curves[[k]][ind,1] - x0
@@ -194,9 +194,9 @@ for(k in 1:n_boot){
     yhat = y1 + p*(y2-y1) + y0
     angles_est_points[i,,k] = c(xhat,yhat)
   }
-  
+
   curve_angles2 = atan((boot_curves2[[k]][,2]-y0)/(boot_curves2[[k]][,1]-x0))
-  
+
   for(i in 1:n_grad){
     ind = min(which(angles[i] >= curve_angles2))
     x1 = boot_curves2[[k]][ind,1] - x0
@@ -223,7 +223,7 @@ for(i in 1:n_grad){
   median[i,] = angles_est_points[i,,order(angles_est_points[i,2,1:n_boot])[n_boot/2]]
   lower_bound[i,] = angles_est_points[i,,order(angles_est_points[i,2,1:n_boot])[n_boot*alpha/2]]
   upper_bound[i,] = angles_est_points[i,,order(angles_est_points[i,2,1:n_boot])[n_boot*(1-alpha/2)]]
-  
+
   median2[i,] = angles_est_points2[i,,order(angles_est_points2[i,2,1:n_boot])[n_boot/2]]
   lower_bound2[i,] = angles_est_points2[i,,order(angles_est_points2[i,2,1:n_boot])[n_boot*alpha/2]]
   upper_bound2[i,] = angles_est_points2[i,,order(angles_est_points2[i,2,1:n_boot])[n_boot*(1-alpha/2)]]
