a5783286cb0627432f17bc3f16262f565d1c06c7
Robert Jane <60888116+rjaneUCF@users.noreply.github.com>
2025-04-10 11:16:09 -0400

    Add files via upload

--- R/Cooley19.R
+++ R/Cooley19.R
@@ -24,31 +24,35 @@
 #' Cooley19(Data=na.omit(S20.Detrend.df[,3:4]),Migpd=s.Migpd,
 #' p.base=0.01,p.proj=0.001,PLOT=TRUE,x_lim_max_T=500,y_lim_max_T=500)
 Cooley19<-function(Data,Migpd,p.base=0.01,p.proj=0.001,u=0.95,PLOT=FALSE,x_lim_min_T=NA, x_lim_max_T=NA,y_lim_min_T=NA,y_lim_max_T=NA,x_lim_min=NA,x_lim_max=NA,y_lim_min=NA,y_lim_max=NA){
-  
+
   #Check the asymptotic properities
   Chi<-chi(Data,nq=1000)
   CHI<-round(Chi$chi[which(abs(Chi$quantile-u)==min(abs(Chi$quantile-u))),2],2)
   n.bar<-(Chi$chibar[which(abs(Chi$quantile-u)==min(abs(Chi$quantile-u)))]+1)/2
-  
+
   #Fit Kernel cumulative survival function to Data
   Fhat <- kcde(Data,gridsize=1000,tail.flag="upper.tail")
-  
+
   #Extract contour
   I.base<-contourLines(x=Fhat$eval.points[[1]], y=Fhat$eval.points[[2]],z=Fhat$estimate, levels=p.base)[[1]]
   I.base <- data.frame(I.base$x,I.base$y)
   colnames(I.base)<-c("x","y")
-  
+
   #Transform observations and contour to frechet (_F) scale
+  #print(EmpFun(x=Data[,1],r=Data[,1], mod=Migpd$models[[1]]))
+
   x_F<- -1/(log(EmpFun(x=Data[,1],r=Data[,1], mod=Migpd$models[[1]])))
   y_F <- -1/(log(EmpFun(x=Data[,2],r=Data[,2], mod=Migpd$models[[2]])))
-  z<-which(x_F==-Inf | y_F==-Inf)
-  x_F<-x_F[-z]
-  y_F<-y_F[-z]
+  #print(which(x_F==-Inf | y_F==-Inf))
+  #z<-which(x_F==-Inf | y_F==-Inf)
+  #x_F<-x_F[-z]
+  #y_F<-y_F[-z]
   I.base.x_F <- -1/(log(EmpFun(x=Data[,1], r=I.base$x, mod=Migpd$models[[1]])))
   I.base.y_F<-  -1/(log(EmpFun(x=Data[,2], r=I.base$y, mod=Migpd$models[[2]])))
   I.base_F <- data.frame(I.base.x_F,I.base.y_F)
   colnames(I.base_F)<-c("x","y")
-  
+  #print(summary(x_F))
+  #print(summary(y_F))
   #Projected isoline
   if(CHI>0){
     I.proj.x_F <- (p.base/p.proj)*I.base.x_F
@@ -61,39 +65,39 @@ Cooley19<-function(Data,Migpd,p.base=0.01,p.proj=0.001,u=0.95,PLOT=FALSE,x_lim_m
   }
   I.proj_F <- data.frame(I.proj.x_F,I.proj.y_F)
   colnames(I.proj_F)<-c("x","y")
-  
+
   #Transforming back to original scale
   I.proj.x_U<- exp(-1/I.proj.x_F)
   I.proj.y_U<- exp(-1/I.proj.y_F)
-  
+
   I.proj.x<-as.numeric(quantile(Data[,1],I.proj.x_U))
   I.proj.x[which(I.proj.x_U>Migpd$models[[1]]$mqu)]<-u2gpd(u=I.proj.x_U[which(I.proj.x_U>Migpd$models[[1]]$mqu)],
                                                            p=Migpd$models[[1]]$rate,
                                                            th=Migpd$models[[1]]$threshold,
                                                            sigma=exp(Migpd$models[[1]]$coefficients[1]),
                                                            xi=Migpd$models[[1]]$coefficients[2])
-  
+
   I.proj.y<-as.numeric(quantile(Data[,2],I.proj.y_U))
   I.proj.y[which(I.proj.y_U>Migpd$models[[2]]$mqu)]<-u2gpd(u=I.proj.y_U[which(I.proj.y_U>Migpd$models[[2]]$mqu)],
                                                            p=Migpd$models[[2]]$rate,
                                                            th=Migpd$models[[2]]$threshold,
                                                            sigma=exp(Migpd$models[[2]]$coefficients[1]),
                                                            xi=Migpd$models[[2]]$coefficients[2])
-  
+
   I.proj <- data.frame(I.proj.x,I.proj.y)
   colnames(I.proj)<-c("x","y")
-  
+
   if(PLOT==TRUE){
     x_lim_min_T<-ifelse(is.na(x_lim_min_T)==T,min(x_F),x_lim_min_T)
     x_lim_max_T<-ifelse(is.na(x_lim_max_T)==T,max(x_F),x_lim_max_T)
     y_lim_min_T<-ifelse(is.na(y_lim_min_T)==T,min(y_F),y_lim_min_T)
     y_lim_max_T<-ifelse(is.na(y_lim_max_T)==T,max(y_F),y_lim_max_T)
-    
+
     x_lim_min<-ifelse(is.na(x_lim_min)==T,min(Data[,1]),x_lim_min)
     x_lim_max<-ifelse(is.na(x_lim_max)==T,max(Data[,1]),x_lim_max)
     y_lim_min<-ifelse(is.na(y_lim_min)==T,min(Data[,2]),y_lim_min)
     y_lim_max<-ifelse(is.na(y_lim_max)==T,max(Data[,2]),y_lim_max)
-    
+
     par(mfrow=c(2,2))
     par(mar=c(4.2,4.2,2,2))
     plot(Chi)
@@ -101,7 +105,7 @@ Cooley19<-function(Data,Migpd,p.base=0.01,p.proj=0.001,u=0.95,PLOT=FALSE,x_lim_m
     legend("topright",c("Base","Proj"),col=c(3,2),lty=1,bg="transparent",bty='n')
     lines(I.base_F$x,I.base_F$y,col=3,lwd=2)
     lines(I.proj_F$x,I.proj_F$y,col=2,lwd=2)
-    
+
     plot(Data,xlim=c(x_lim_min,x_lim_max),ylim=c(y_lim_min,y_lim_max),main="Original scale")
     legend("topright",c("Base","Proj"),col=c(3,2),lty=1,bg="transparent",bty='n')
     lines(I.base$x,I.base$y,col=3,lwd=2)
