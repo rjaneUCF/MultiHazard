1d4fe3046944d8411e07f971ef9f56288074c71c
Robert Jane <60888116+rjaneUCF@users.noreply.github.com>
2025-05-27 13:58:44 -0400

    Update HURDAT.R

--- R/HURDAT.R
+++ R/HURDAT.R
@@ -29,7 +29,7 @@ HURDAT <- function(Data,lat.loc,lon.loc,rad){
 
   #Combining name vecor with HURDAT2 data
   HURDAT2 = data.frame(storm.names, HURDAT2[-which(n.char==37),])
-  print(summary(HURDAT2))
+  print(head(HURDAT2))
   #Functions to extract (1) latitude and (2) longitude of storm
   lat.cal = function(x) as.numeric(trimws(substring(x[[2]],24,27)))
   lon.cal = function(x) as.numeric(trimws(substring(x[[2]],30,35)))
