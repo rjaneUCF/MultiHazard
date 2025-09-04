6f1f606217d08b25b0d0544da71f473504dfcca0
Robert Jane <60888116+rjaneUCF@users.noreply.github.com>
2025-09-04 18:33:39 -0400

    Add files via upload

--- R/HT04.R
+++ R/HT04.R
@@ -21,7 +21,7 @@
 #' @seealso \code{\link{Dataframe_Combine}} \code{\link{Migpd_Fit}}
 #' @export
 #' @examples
-#' Fit GPD marginal distributions above the threshold
+#' #Fit GPD marginal distributions above the threshold
 #' S20.Migpd<-Migpd_Fit(Data=S20.Detrend.Declustered.df[,-1],
 #'                      Data_Full=S20.Detrend.Declustered.df[,-1],
 #'                      mqu =c(0.99,0.99,0.99))
