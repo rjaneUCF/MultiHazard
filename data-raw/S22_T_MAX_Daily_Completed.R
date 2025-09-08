1eed348d25664b5d36915f91053b2358438aff59
Robert Jane <60888116+rjaneUCF@users.noreply.github.com>
2025-09-08 13:37:03 -0400

    Add files via upload

--- data-raw/S22_T_MAX_Daily_Completed.R
+++ data-raw/S22_T_MAX_Daily_Completed.R
@@ -1,2 +1,2 @@
-S22_T_MAX_Daily_Completed<-read.csv("S22_T_MAX_Daily_Completed.csv",header=TRUE)[,c(2,3)]
+S22_T_MAX_Daily_Completed<-read.mat("S22_T_MAX_Daily_Completed.csv")
 devtools::use_data(S22_T_MAX_Daily_Completed)
