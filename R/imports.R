32c90705446db09e4492df2c4875bb5855a41556
Robert Jane <60888116+rjaneUCF@users.noreply.github.com>
2025-09-29 19:49:14 -0400

    Update imports.R

--- R/imports.R
+++ R/imports.R
@@ -16,10 +16,10 @@
 #' @import truncnorm
 #' @import tweedie
 #' @importFrom utils capture.output
-#' @importFrom VGAM vglm dbisa pbisa
+#' @importFrom VGAM vglm dbisa pbisa bisa
 #' @importFrom lubridate year
 #' @importFrom VineCopula RVineMatrix BiCopEst BiCopSim RVineSim BiCopSelect
 #' @importFrom gamlss gamlss
 #' @importFrom gamlss.mx gamlssMX dMX pMX qMX
-#' @importFrom gamlss.dist GU RU GG GA dGG pGG dGA pGA
+#' @importFrom gamlss.dist GU RU GG GA dGG pGG dGA pGA dGU pGU dRU pRU
 "_PACKAGE"
