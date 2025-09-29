11c37f8ec5ee321f73d9e952d31ca242bb4cef27
Robert Jane <60888116+rjaneUCF@users.noreply.github.com>
2025-09-29 13:55:17 -0400

    Add files via upload

--- R/imports.R
+++ R/imports.R
@@ -6,4 +6,7 @@
 #' @importFrom VGAM vglm dbisa pbisa
 #' @importFrom lubridate year
 #' @importFrom VineCopula RVineMatrix BiCopEst BiCopSim RVineSim
+#' @importFrom gamlss gamlss
+#' @importFrom gamlss.mx gamlssMX dMX pMX
+#' @importFrom gamlss.dist GG GA dGG pGG dGA pGA
 "_PACKAGE"
