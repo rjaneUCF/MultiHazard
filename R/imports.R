398bac48ec1a3c5c857902a717ba1bc5e1191b6e
Robert Jane <60888116+rjaneUCF@users.noreply.github.com>
2025-09-30 12:21:15 -0400

    Add files via upload

--- R/imports.R
+++ R/imports.R
@@ -18,7 +18,7 @@
 #' @importFrom utils capture.output
 #' @importFrom VGAM Coef vglm dbisa pbisa qbisa bisa dlaplace plaplace qlaplace
 #' @importFrom lubridate year month
-#' @importFrom VineCopula RVineMatrix BiCopEst BiCopSim RVineSim BiCopSelect BiCopCDF
+#' @importFrom VineCopula RVineMatrix BiCopEst BiCopSim RVineSim BiCopSelect BiCopCDF RVineCopSelect RVineStructureSelect
 #' @importFrom gamlss gamlss
 #' @importFrom gamlss.mx gamlssMX dMX pMX
 #' @importFrom gamlss.dist GU GG GA RG dGG pGG dGA pGA dGU pGU dRG qRG pRG qGU qGG qGA
