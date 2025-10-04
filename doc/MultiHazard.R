8fdb53b9339a029130164c11b214ca2c6a988582
Robert Jane <60888116+rjaneUCF@users.noreply.github.com>
2025-10-04 10:48:49 -0400

    Add files via upload

--- doc/MultiHazard.R
+++ doc/MultiHazard.R
@@ -1,7 +1,13 @@
 ## ----setup, include=FALSE-----------------------------------------------------
-knitr::opts_chunk$set(echo = TRUE, warning = FALSE, message = FALSE, collapse = TRUE,
-  comment = "#>")
-fig.path = "../man/figures/vignette-"
+knitr::opts_chunk$set(
+  echo = TRUE, 
+  warning = FALSE, 
+  message = FALSE, 
+  collapse = TRUE,
+  comment = "#>",
+  fig.path = "figures/"
+)
+
 library(MultiHazard)
 library(dplyr)
 library(scales)
@@ -76,126 +82,4 @@ abline(h=G_3356.Declustered$Threshold,col="Dark Green")
 points(as.Date(G_3356_Imp$Date[G_3356.Declustered$EventsMax]),
        G_3356.Declustered$Declustered[G_3356.Declustered$EventsMax],
        col="Red",pch=16,cex=0.5)
-
-## -----------------------------------------------------------------------------
-G_3356.Declustered$Threshold
-
-## -----------------------------------------------------------------------------
-G_3356.Declustered$Rate
-
-## -----------------------------------------------------------------------------
-S20.Groundwater.Detrend.Declustered<-G_3356.Declustered$Declustered
-
-## -----------------------------------------------------------------------------
-#Changing names of the data frames
-S20.Rainfall.df<-Perrine_df
-S20.OsWL.df<-S20_T_MAX_Daily_Completed_Detrend_Declustered[,c(1,3)]
-#Converting Date column to "Date"" object
-S20.Rainfall.df$Date<-as.Date(as.character(S20.Rainfall.df$Date), format = "%m/%d/%Y")
-S20.OsWL.df$Date<-as.Date(as.character(S20.OsWL.df$Date), format = "%m/%d/%Y")
-
-## -----------------------------------------------------------------------------
-S20.OsWL.Detrend<-Detrend(Data=S20.OsWL.df,Method = "window",PLOT=FALSE,
-                          x_lab="Date",y_lab="O-sWL (ft NGVD 29)")
-
-## -----------------------------------------------------------------------------
-S20.OsWL.Detrend.df<-data.frame(as.Date(S20.OsWL.df$Date),S20.OsWL.Detrend)
-colnames(S20.OsWL.Detrend.df)<-c("Date","OsWL")
-
-## -----------------------------------------------------------------------------
-#Declustering rainfall and O-sWL series
-S20.Rainfall.Declustered<-Decluster(Data=S20.Rainfall.df$Value,u=0.95,SepCrit=3)$Declustered
-S20.OsWL.Detrend.Declustered<-Decluster(Data=S20.OsWL.Detrend,u=0.95,SepCrit=3,mu=365.25)$Declustered
-
-## -----------------------------------------------------------------------------
-S20.OsWL.Detrend.Declustered.df<-data.frame(S20.OsWL.df$Date,S20.OsWL.Detrend.Declustered)
-colnames(S20.OsWL.Detrend.Declustered.df)<-c("Date","OsWL")
-S20.Rainfall.Declustered.df<-data.frame(S20.Rainfall.df$Date,S20.Rainfall.Declustered)
-colnames(S20.Rainfall.Declustered.df)<-c("Date","Rainfall")
-S20.Groundwater.Detrend.Declustered.df<-data.frame(G_3356$Date,S20.Groundwater.Detrend.Declustered)
-colnames(S20.Groundwater.Detrend.Declustered.df)<-c("Date","Groundwater")
-
-## -----------------------------------------------------------------------------
-S20.Detrend.df<-Dataframe_Combine(data.1<-S20.Rainfall.df,
-                                  data.2<-S20.OsWL.Detrend.df,
-                                  data.3<-S20.Groundwater.Detrend.df,
-                                  names=c("Rainfall","OsWL","Groundwater"), n=3)
-S20.Detrend.Declustered.df<-Dataframe_Combine(data.1<-S20.Rainfall.Declustered.df,
-                                              data.2<-S20.OsWL.Detrend.Declustered.df,
-                                              data.3<-S20.Groundwater.Detrend.Declustered.df,
-                                              names=c("Rainfall","OsWL","Groundwater"), n=3)
-
-## -----------------------------------------------------------------------------
-S20.Rainfall.Declustered.SW<-Decluster_SW(Data=S20.Rainfall.df,Window_Width=7)
-
-## -----------------------------------------------------------------------------
-plot(S20.Rainfall.df$Date,S20.Rainfall.df$Value,pch=16,cex=0.5,
-     xlab="Date",ylab="Total daily rainfall (Inches)")
-#points(S20.Rainfall.df$Date,S20.Rainfall.Declustered.SW$Declustered,pch=16,col=2,cex=0.5)
-
-## -----------------------------------------------------------------------------
-S20.OsWL.Declustered.SW<-Decluster_SW(Data=S20.OsWL.df,Window_Width=3)
-
-## -----------------------------------------------------------------------------
-#Declustering
-S20.Rainfall.Declustered.S.SW<-Decluster_S_SW(Data=S20.Rainfall.df, 
-                                              Window_Width_Sum=7, Window_Width=7)
-#First twenty values of the weekly totals
-S20.Rainfall.Declustered.S.SW$Totals[1:20]
-
-## -----------------------------------------------------------------------------
-#First ten values of the declustered weekly totals
-S20.Rainfall.Declustered.S.SW$Declustered[1:20]
-
-## -----------------------------------------------------------------------------
-plot(S20.Rainfall.df$Date,S20.Rainfall.Declustered.S.SW$Totals,pch=16,cex=0.5,
-     xlab="Date",ylab="Total weekly rainfall (Inches)")
-points(S20.Rainfall.df$Date,S20.Rainfall.Declustered.S.SW$Declustered,pch=16,col=2,cex=0.5)
-
-## -----------------------------------------------------------------------------
-GPD_Fit(Data=S20.Detrend.Declustered.df$Rainfall,Data_Full=na.omit(S20.Detrend.df$Rainfall),
-        u=0.997,PLOT=TRUE,xlab_hist="Rainfall (Inches)",y_lab="Rainfall (Inches)")
-
-## -----------------------------------------------------------------------------
-S20.Rainfall.Solari<-GPD_Threshold_Solari(Event=S20.Rainfall.Declustered.SW$Declustered,
-                                          Data=S20.Detrend.df$Rainfall)
-
-## -----------------------------------------------------------------------------
-S20.Rainfall.Solari$Candidate_Thres
-
-## -----------------------------------------------------------------------------
-Rainfall.Thres.Quantile<-ecdf(S20.Detrend.df$Rainfall)(S20.Rainfall.Solari$Candidate_Thres)
-
-## -----------------------------------------------------------------------------
-Solari.Sel<-GPD_Threshold_Solari_Sel(Event=S20.Rainfall.Declustered.SW$Declustered,
-                                    Data=S20.Detrend.df$Rainfall,
-                                    Solari_Output=S20.Rainfall.Solari,
-                                    Thres=S20.Rainfall.Solari$Candidate_Thres,
-                                    RP_Max=100)
-
-## ----warning=FALSE, message=FALSE, error=FALSE--------------------------------
-S20.OsWL.Solari<-GPD_Threshold_Solari(Event=S20.OsWL.Declustered.SW$Declustered,
-                                      Data=S20.Detrend.df$OsWL)
-
-## -----------------------------------------------------------------------------
-S20.OsWL.Solari$Candidate_Thres
-
-## -----------------------------------------------------------------------------
-OsWL.Thres.Quantile<-ecdf(S20.Detrend.df$OsWL)(S20.OsWL.Solari$Candidate_Thres)
-
-## -----------------------------------------------------------------------------
-Solari.Sel<-GPD_Threshold_Solari_Sel(Event=S20.OsWL.Declustered.SW$Declustered,
-                                     Data=S20.Detrend.df$OsWL,
-                                     Solari_Output=S20.OsWL.Solari,
-                                     Thres=S20.OsWL.Solari$Candidate_Thres,
-                                     RP_Max=100)
-
-## -----------------------------------------------------------------------------
-S20.Kendall.Results<-Kendall_Lag(Data=S20.Detrend.df,GAP=0.2)
-
-## -----------------------------------------------------------------------------
-S20.Kendall.Results$Value$Rainfall_OsWL
-
-## -----------------------------------------------------------------------------
-S20.Kendall.Results$Test$Rainfall_OsWL_Test
 
