7324e329b944754eb09ee22629acb6391a599ad7
Robert Jane <60888116+rjaneUCF@users.noreply.github.com>
2025-10-05 10:03:46 -0400

    Add files via upload

--- R/NOAA_SLR.R
+++ R/NOAA_SLR.R
@@ -5,13 +5,13 @@
 #' @param OsWL_req Numeric vector of SLR required.
 #' @param SLR_scen Character vector specifying which of the NOAA (2017) scenarios to consider. Options include \code{High}, Intermediate high \code{Int.High}, \code{Intermediate}, Intermediate low (\code{Int.Low}) and \code{Low}.
 #' @param Input_unit Character vector of length one; specifying units of SLR. Default is meters \code{"m"}, other option is feet \code{"ft"}.
-#' @param Year.Inital Character vector of length one; specifying the current year.
+#' @param Year.Initial Character vector of length one; specifying the current year.
 #' @return List comprising the specified \code{Threshold} as the quantile of the conditioning variable above which declustered excesses are paired with co-occurrences of the other variable, the resulting two-dimensional sample \code{data} and \code{name} of the conditioning variable.
 #' @export
 #' @examples
 #' NOAA_SLR(OsWL_req=seq(0,1,0.01),SLR_scen = c("High","Intermediate","Low"),
 #'                    Input_unit="m")
-NOAA_SLR<-function(OsWL_req,SLR_scen = c("High","Intermediate","Low"),Input_unit="m",Year.Inital=2020){
+NOAA_SLR<-function(OsWL_req,SLR_scen = c("High","Intermediate","Low"),Input_unit="m",Year.Initial=2020){
   res<-matrix(0,nrow=length(OsWL_req),ncol=length(SLR_scen))
   scenario<-which(colnames(NOAAetal2017)==SLR_scen)
   for(j in 1:length(scenario)){
