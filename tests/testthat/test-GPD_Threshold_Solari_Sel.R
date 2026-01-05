dac9d395bf7397f993cb85c72d3d0f32dedd4ada
Robert Jane <60888116+rjaneUCF@users.noreply.github.com>
2026-01-05 13:16:05 -0500

    Add files via upload

--- tests/testthat/test-GPD_Threshold_Solari_Sel.R
+++ tests/testthat/test-GPD_Threshold_Solari_Sel.R
@@ -1,16 +1,18 @@
 Rainfall_Declust_SW<-Decluster_SW(Data=S22.Detrend.df[24473:25568,c(1:2)],Window_Width=7)
 
 #Finding an appropriate threshold for the declustered series
-S22_OsWL_Solari<-suppressWarnings(GPD_Threshold_Solari(Event=Rainfall_Declust_SW$Declustered,
-                                     Data=na.omit(S22.Detrend.df[24473:25568,2])))
+S22_OsWL_Solari<-suppressWarnings(
+                     suppressMessages(GPD_Threshold_Solari(Event=Rainfall_Declust_SW$Declustered,
+                                     Data=na.omit(S22.Detrend.df[24473:25568,2]))))
 
 
 test_that("GPD_Threshold_Solari_Sel works", {
 
-  result <- suppressWarnings(GPD_Threshold_Solari_Sel(Event=Rainfall_Declust_SW$Declustered,
+  result <- suppressWarnings(
+                   suppressMessages(GPD_Threshold_Solari_Sel(Event=Rainfall_Declust_SW$Declustered,
                                      Data=S22.Detrend.df[24473:25568,2],
                                      Solari_Output=S22_OsWL_Solari,
-                                     Thres=S22_OsWL_Solari$Candidate_Thres))
+                                     Thres=S22_OsWL_Solari$Candidate_Thres)))
 
   # Checking type of output
   expect_type(result, 'list')
@@ -26,17 +28,19 @@ test_that("GPD_Threshold_Solari_Sel works", {
 
 test_that("Invalid inputs", {
 
-  expect_error(
+  expect_error(suppressWarnings(
+    suppressMessages(
     GPD_Threshold_Solari_Sel(Event=Rainfall_Declust_SW$Declustered,
                              Data=S22.Detrend.df[24473:25568,2],
                              Solari_Output = NULL,
-                             Thres=S22_OsWL_Solari$Candidate_Thres),
-    "Solari_Output is missing.")
+                             Thres=S22_OsWL_Solari$Candidate_Thres))),
+                              "Solari_Output is missing.")
 
-  expect_error(
-    GPD_Threshold_Solari_Sel(Event=Rainfall_Declust_SW$Declustered,
-                             Data=S22.Detrend.df[24473:25568,2],
-                             Solari_Output=S22_OsWL_Solari,
-                             Thres=S22_OsWL_Solari$Candidate_Thres,RP_Max=-50),
-    "RP_Max must be a single positive numeric value.")
+  expect_error(suppressWarnings(
+                  suppressMessages(
+                       GPD_Threshold_Solari_Sel(Event=Rainfall_Declust_SW$Declustered,
+                                                Data=S22.Detrend.df[24473:25568,2],
+                                                Solari_Output=S22_OsWL_Solari,
+                                                Thres=S22_OsWL_Solari$Candidate_Thres,RP_Max=-50))),
+                          "RP_Max must be a single positive numeric value.")
 })
